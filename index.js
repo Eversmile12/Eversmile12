f13ac15c954c0a79228c1a36122b1286b2f59fe1
Vittorio Rivabella <20019426@uhi.ac.uk>
2021-01-26 19:25:24 +0100

    sorting terracare badge e reimporting npm pack

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// const mustache = require('mustache');
-// const fs = require('fs');
-// const fetch = require('node-fetch');
-// const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const mustache = require('mustache');
+const fs = require('fs');
+const fetch = require('node-fetch');
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const MUSTACHE_MAIN_DIR = "./main.mustache";
 const http = new XMLHttpRequest();
 
@@ -27,23 +27,23 @@ function fetchLatestArticles(){
             }),
             first_post_title: response["post1"]["post-title"],
             first_post_content: response["post1"]["post-content"],
-            first_post_date: response["post1"]["post-date"],
+            first_post_date: response["post1"]["post-date"].split(" ")[0],
             first_post_url: response["post1"]["post-url"],
             second_post_title: response["post2"]["post-title"],
             second_post_content: response["post2"]["post-content"],
             second_post_date: response["post2"]["post-date"].split(" ")[0],
             second_post_url: response["post2"]["post-url"],
         }
-        // fs.readFile(MUSTACHE_MAIN_DIR,(err, data)=>{
-        //     if(err){
-        //         console.log(err);
-        //         throw(err);
-        //     }else{
-        //         const output = mustache.render(data.toString(), dataToRender);
-        //         fs.writeFileSync('README.MD', output);
-        //         console.log("readme created");
-        //     }
-        // })
+        fs.readFile(MUSTACHE_MAIN_DIR,(err, data)=>{
+            if(err){
+                console.log(err);
+                throw(err);
+            }else{
+                const output = mustache.render(data.toString(), dataToRender);
+                fs.writeFileSync('README.MD', output);
+                console.log("readme created");
+            }
+        })
         return http.responseText;
     }
 }
